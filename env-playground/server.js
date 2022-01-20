685ad4ce314b921d4f08bb19c6cddcdace8b5785
szabikr <szabikr.dev@gmail.com>
2022-01-20 22:58:20 +0000

    env-playground: implementing simple http server

--- env-playground/server.js
+++ env-playground/server.js
@@ -1,2 +1,12 @@
-console.log(`Running a ${process.env.NODE_ENV} environment`);
-console.log(`Your PORT is ${process.env.PORT}`);
+const http = require('http');
+
+const requestListener = function (req, res) {
+  console.log(`Received ${req.method} type request`);
+  res.writeHead(200);
+  res.end(`Hello from ${process.env.NODE_ENV} environment!`);
+}
+
+const server = http.createServer(requestListener);
+server.listen(process.env.PORT);
+
+console.log(`Server listening on port ${process.env.PORT} in ${process.env.NODE_ENV} environment ...`);
